4bc56868b7c7c1daf73679d9d18ca5e6926d0313
Ben Chatelain <ben@octop.ad>
2023-12-07 08:21:07 -0700

    🔊 Log DittoReactNativeVersionNumber

--- ios/DittoReactNativeExampleIOS/AppDelegate.mm
+++ ios/DittoReactNativeExampleIOS/AppDelegate.mm
@@ -1,6 +1,9 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+//#import "DittoRNSDK.h"
+#import <DittoReactNative/DittoReactNative.h>
+#import <dittoffi.h>
 
 @implementation AppDelegate
 
@@ -10,6 +13,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  NSLog(@"DittoReactNativeVersionNumber: %lf", DittoReactNativeVersionNumber);
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
